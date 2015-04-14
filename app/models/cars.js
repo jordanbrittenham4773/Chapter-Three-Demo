921357aaae59fb679f19a9ddf2b5705e34107208
unknown <jb936983@CC205_27.wtstudents.wtamu.edu>
2015-04-14 16:30:04 -0500

    Initial Commit

---
+++ app/models/cars.js
@@ -0,0 +1,21 @@
+exports.definition = {
+	config: {
+
+		adapter: {
+			type: "properties",
+			collection_name: "cars"
+		}
+	},
+	extendModel: function(Model) {
+		_.extend(Model.prototype, {
+		});
+
+		return Model;
+	},
+	extendCollection: function(Collection) {
+		_.extend(Collection.prototype, {
+		});
+
+		return Collection;
+	}
+};
